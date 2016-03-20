c02691ca5f9afe030a3c902718e12ab39352140a
Kyle Malfatti <kyle.malfatti@gmail.com>
2016-03-19 17:29:09 -0700

    added randomcolors

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Your JS goes here
+
+function rand() {
+  return "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
+}
+for(var i = 0; i < 81; i++) {
+var square = document.createElement('div');
+var black = document.querySelector('body').appendChild(square).style;
+black.backgroundColor = rand();
+black.width = "11.1%";
+black.paddingBottom = "11.1%";
+black.float = "left";
+}
