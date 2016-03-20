676ad058321029f08191a368e80a69af10ed3e65
Kyle Malfatti <kyle.malfatti@gmail.com>
2016-03-19 17:14:55 -0700

    added blk/red checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// Your JS goes here
+for(var i = 0; i < 40; i++) {
+var square = document.createElement('div');
+var black = document.querySelector('body').appendChild(square).style;
+black.backgroundColor = "black";
+black.width = "11.1%";
+black.paddingBottom = "11.1%";
+black.float = "left";
+var square = document.createElement('div');
+var red = document.querySelector('body').appendChild(square).style;
+red.backgroundColor = "red";
+red.width = "11.1%";
+red.paddingBottom = "11.1%";
+red.float = "left";
+}
+// Uneven board. Necessary for last black square
+var square = document.createElement('div');
+var black = document.querySelector('body').appendChild(square).style;
+black.backgroundColor = "black";
+black.width = "11.1%";
+black.paddingBottom = "11.1%";
+black.float = "left";
