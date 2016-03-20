d3a83b12142135cb2a0e2449de5a86c25b925b7b
Kyle Malfatti <kyle.malfatti@gmail.com>
2016-03-20 14:28:04 -0700

    added gradient

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 // Your JS goes here
 
-function rand() {
-  return "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
-}
-for(var i = 0; i < 81; i++) {
+for(var i = 0; i < 40; i++) {
 var square = document.createElement('div');
 var black = document.querySelector('body').appendChild(square).style;
-black.backgroundColor = rand();
+black.backgroundColor = "black";
 black.width = "11.1%";
 black.paddingBottom = "11.1%";
 black.float = "left";
+black.opacity = 0.2;
+var square = document.createElement('div');
+var red = document.querySelector('body').appendChild(square).style;
+red.backgroundColor = "red";
+red.width = "11.1%";
+red.paddingBottom = "11.1%";
+red.float = "left";
+red.opacity = 0.4;
 }
+
+var square = document.createElement('div');
+var black = document.querySelector('body').appendChild(square).style;
+black.backgroundColor = "black";
+black.width = "11.1%";
+black.paddingBottom = "11.1%";
+black.float = "left";
+black.opacity = 0.2;
+
+document.querySelector('body').style.background = 'linear-gradient(135deg, blue, yellow)';
