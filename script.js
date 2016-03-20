2f66350ebd43c35109760d2ad14dd88504b31f61
Kyle Malfatti <kyle.malfatti@gmail.com>
2016-03-20 16:01:21 -0700

    added bonus audio

--- script.js
+++ script.js
@@ -14,4 +14,18 @@ black.paddingBottom = "11.1%";
 black.float = "left";
 }
 }
+
+//Bonus Audio
+//Is there a way to use audio from soundcloud or a similar website?
+//I tried hotlinking from different places but failed,
+//so I had to use my own file I downloaded for the music.
+//slack - kmalfatti 
+//Thanks!
+var audio = document.createElement('audio');
+audio.setAttribute('autoplay', "");
+var source = document.createElement('source');
+source.setAttribute('src', '/Users/kmalfatti/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/Goldeneye 007 (Music) - Facility.mp3');
+audio.appendChild(source);
+document.querySelector('body').appendChild(audio);
+
 setInterval(cb, 2000);
